9c177577585de95797e4e3c24e01502008359e73
Vladimír Štusák <Vladimir.Stusak@ysoft.com>
2020-10-11 23:11:04 +0200

    SRP + homework

---
+++ Lesson1-SRP/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace Lesson1_SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var retirementCalculator = new RetirementCalculator();
+
+            //retirementCalculator.GenerateSalaries();
+            retirementCalculator.Process();
+
+            Console.WriteLine(retirementCalculator.RetirementSalary > 20000
+                ? "Congratulations and have a nice retirement"
+                : "You will need additional work now or in retirement, sorry");
+        }
+    }
+
+    public class RetirementCalculator
+    {
+        public int RetirementSalary { get; set; }
+
+        public void Process()
+        {
+            var baseSalary = 10000;
+            double multiplication = 1;
+            var bonuses = new List<int>();
+
+            var salaries = JsonSerializer.Deserialize<IEnumerable<Salary>>(File.ReadAllText("salaries.json")).ToList();
+
+            if (salaries.Count() > 50)
+            {
+                multiplication += 0.3;
+            }
+
+            if (salaries.Select(salary => salary.Value).Average() > 30000)
+            {
+                multiplication += 1;
+            }
+
+            if (salaries.Select(salary => salary.Value).Any(value => value > 47000))
+            {
+                bonuses.Add(2000);
+            }
+
+            RetirementSalary = Convert.ToInt32(baseSalary * multiplication + bonuses.Sum());
+        }
+
+        public void GenerateSalaries()
+        {
+            var salaryGenerator = new Random();
+
+            var salaries = new List<Salary>();
+            for (int i = 0; i < 100; i++)
+            {
+                salaries.Add(new Salary
+                    { DateTime = new DateTime(2020, 9, 13).AddMonths(i * -1), Value = salaryGenerator.Next(5000, 50000) });
+            }
+
+            var json = JsonSerializer.Serialize(salaries);
+            File.AppendAllText("salaries.json", json);
+        }
+    }
+
+    public  class Salary
+    {
+        public DateTime DateTime { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ SOLID/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set Command (+. -, *, /");
+            var key = Console.ReadKey();
+
+            switch (key.KeyChar)
+            {
+                case '+':
+                    Add();
+                    break;
+                case '-':
+                    Sub();
+                    break;
+                case '*':
+                    Mul();
+                    break;
+                case '/':
+                    Div();
+                    break;
+                default:
+                    Console.WriteLine("Not supported");
+                    break;
+            }
+        }
+
+        private static void Add()
+        {
+            Console.WriteLine("\nset value 1");
+            var value1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("set value 2");
+            var value2 = int.Parse(Console.ReadLine());
+
+            var result = value1 + value2;
+
+            var output = $"{value1} + {value2} = {result}";
+            Console.WriteLine(output);
+            LogHistory(output);
+        }
+
+        private static void Sub()
+        {
+            Console.WriteLine("\nset value 1");
+            var value1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("set value 2");
+            var value2 = int.Parse(Console.ReadLine());
+
+            var result = value1 - value2;
+
+            var output = $"{value1} - {value2} = {result}";
+            Console.WriteLine(output);
+            LogHistory(output);
+        }
+
+        private static void Mul()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Div()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void LogHistory(string output)
+        {
+            File.AppendAllText("log.json", $"{DateTime.UtcNow} : {output}\n");
+        }
+    }
+}
