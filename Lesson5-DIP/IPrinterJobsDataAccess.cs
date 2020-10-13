86994e04c51cc4a8172878bf0826b63e1c48fb36
Vladimír Štusák <Vladimir.Stusak@ysoft.com>
2020-10-13 11:32:15 +0200

    DIP

---
+++ Lesson5-DIP/IPrinterJobsDataAccess.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson5_DIP
+{
+    public interface IPrinterJobsDataAccess
+    {
+        SqlDataReader GetJobs(SqlParameterCollection parameters);
+    }
+    
+    public class SqlParameterCollection
+    {
+    }
+
+    public class SqlDataReader
+    {
+    }
+}
---
+++ Lesson5-DIP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson5_DIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    
+}
