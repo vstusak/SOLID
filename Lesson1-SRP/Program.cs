ab3671d7a888e78b5d7861a60b8a854f582ca655
Vladimír Štusák <Vladimir.Stusak@ysoft.com>
2020-10-13 23:30:00 +0200

    typo

--- Lesson1-SRP/Program.cs
+++ Lesson1-SRP/Program.cs
@@ -67,7 +67,7 @@ namespace Lesson1_SRP
         }
     }
 
-    public  class Salary
+    public class Salary
     {
         public DateTime DateTime { get; set; }
         public int Value { get; set; }
