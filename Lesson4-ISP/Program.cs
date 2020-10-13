bfc3a51cf5d72b7016d631e022c39fe099bfdeea
Vladimír Štusák <Vladimir.Stusak@ysoft.com>
2020-10-13 02:09:40 +0200

    ISP

---
+++ Lesson4-ISP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson4_ISP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface INotificationService
+    {
+        void SendEmail(string address, string message);
+        void SendEmail(IEnumerable<string> addresses, string message);
+        void SendText(string phoneNumber, string message);
+        void SendPostcard(string name, string street, string town, string message);
+        void ShowPopup(string clientUri, string message);
+    }
+}
