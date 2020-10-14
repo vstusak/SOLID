46e7951ed89b5b5ab876d5fb7666f0ec20656795
Vladimír Štusák <Vladimir.Stusak@ysoft.com>
2020-10-14 20:37:57 +0200

    1st lesson

--- Lesson1-SRP/Program.cs
+++ Lesson1-SRP/Program.cs
@@ -11,13 +11,50 @@ namespace Lesson1_SRP
         static void Main(string[] args)
         {
             var retirementCalculator = new RetirementCalculator();
+            var logger = new Logger();
 
-            //retirementCalculator.GenerateSalaries();
-            retirementCalculator.Process();
+            var salaries = new SalariesGenerator().Load();
+            retirementCalculator.Process(salaries);
 
-            Console.WriteLine(retirementCalculator.RetirementSalary > 20000
+            logger.Log(retirementCalculator.RetirementSalary > 20000
                 ? "Congratulations and have a nice retirement"
                 : "You will need additional work now or in retirement, sorry");
+        }
+    }
+
+    public class SalariesProvider
+    {
+        public virtual IEnumerable<Salary> Load()
+        {
+            //using system.io.abstraction
+            return JsonSerializer.Deserialize<IEnumerable<Salary>>(File.ReadAllText("salaries.json"));
+        }
+    }
+
+    public class SalariesGenerator : SalariesProvider
+    {
+        public override IEnumerable<Salary> Load()
+        {
+            var salaryGenerator = new Random();
+
+            var salaries = new List<Salary>();
+            for (int i = 0; i < 100; i++)
+            {
+                salaries.Add(new Salary
+                    { DateTime = new DateTime(2020, 9, 13).AddMonths(i * -1), Value = salaryGenerator.Next(5000, 50000) });
+            }
+
+            return salaries;
+            //var json = JsonSerializer.Serialize(salaries);
+            //File.AppendAllText("salaries.json", json);
+        }
+    }
+
+    internal class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 
@@ -25,14 +62,12 @@ namespace Lesson1_SRP
     {
         public int RetirementSalary { get; set; }
 
-        public void Process()
+        public void Process(IEnumerable<Salary> salaries)
         {
             var baseSalary = 10000;
             double multiplication = 1;
             var bonuses = new List<int>();
-
-            var salaries = JsonSerializer.Deserialize<IEnumerable<Salary>>(File.ReadAllText("salaries.json")).ToList();
-
+            
             if (salaries.Count() > 50)
             {
                 multiplication += 0.3;
@@ -51,20 +86,7 @@ namespace Lesson1_SRP
             RetirementSalary = Convert.ToInt32(baseSalary * multiplication + bonuses.Sum());
         }
 
-        public void GenerateSalaries()
-        {
-            var salaryGenerator = new Random();
-
-            var salaries = new List<Salary>();
-            for (int i = 0; i < 100; i++)
-            {
-                salaries.Add(new Salary
-                    { DateTime = new DateTime(2020, 9, 13).AddMonths(i * -1), Value = salaryGenerator.Next(5000, 50000) });
-            }
-
-            var json = JsonSerializer.Serialize(salaries);
-            File.AppendAllText("salaries.json", json);
-        }
+        
     }
 
     public class Salary
