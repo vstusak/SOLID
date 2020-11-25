818706cb09f50462f22f2dbef616be2add51a571
Petr Pucek <ppucek@moravia.com>
2020-11-25 13:11:59 +0100

    LSP - rectangle

--- Lesson3-LSP/Program.cs
+++ Lesson3-LSP/Program.cs
@@ -6,24 +6,32 @@ namespace Lesson3_LSP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rectangle = new Square();
+            rectangle.Height = 5;
+            rectangle.Width = 9;
+
+            Console.WriteLine(rectangle.IsSquare ? "Is square." : "Is rectangle.");
+            Console.WriteLine(rectangle.Area);
         }
 
         public class Rectangle
         {
-            
+            public virtual int Width { get; set; }
+            public virtual int Height { get; set; }
+            public bool IsSquare => Height == Width;
+            public int Area => Height * Width;
         }
 
-        public class Square
+        public class Square : Rectangle
         {
-
+            
         }
 
         public class Calculator
         {
-            public int CalculateArea()
+            public int CalculateArea(Rectangle rectangle)
             {
-                return 0;
+                return rectangle.Width * rectangle.Height;
             }
         }
     }
