f30a566aa47d22f67010165e177af11bb4bc2a5e
LHybst <LHybst@gmail.com>
2020-12-02 10:46:36 +0100

    Added missing interface changes

--- Lesson5-DIP/IPrinterJobsDataAccess.cs
+++ Lesson5-DIP/IPrinterJobsDataAccess.cs
@@ -1,15 +1,36 @@
-﻿namespace Lesson5_DIP
+﻿using System.Collections.Generic;
+
+namespace Lesson5_DIP
 {
     public interface IPrinterJobsDataAccess
     {
-        SqlDataReader GetJobs(SqlParameterCollection parameters);
+        IEnumerable<Jobs> GetJobs(IDictionary<string, string> parameters);
     }
-    
+
+    public class Jobs
+    {
+    }
+
     public class SqlParameterCollection
     {
     }
 
     public class SqlDataReader
     {
+    }
+
+    public class PrinterJobsDataAccess : IPrinterJobsDataAccess
+    {
+        private SqlDataReader _dataReader;
+
+        public PrinterJobsDataAccess(SqlDataReader dataReader)
+        {
+            _dataReader = dataReader;
+        }
+
+        public IEnumerable<Jobs> GetJobs(IDictionary<string, string> parameters)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
