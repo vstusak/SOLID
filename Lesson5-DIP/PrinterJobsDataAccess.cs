a5c39a0f94ce5f5e2b0235e495ad0d1b830ae728
Vladimír Štusák <Vladimir.Stusak@ysoft.com>
2020-12-11 07:36:37 +0100

    build fix

--- Lesson5-DIP/PrinterJobsDataAccess.cs
+++ Lesson5-DIP/PrinterJobsDataAccess.cs
@@ -1,28 +1,28 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.IO.Abstractions;
+//using System.IO.Abstractions;
 
 namespace Lesson5_DIP
 {
     public class PrinterJobsDataAccess : IPrinterJobsDataAccess
     {
         private SqlDataReader _dataReader;
-        private readonly IFileSystem _fileSystem;
+        //private readonly IFileSystem _fileSystem;
 
-        public PrinterJobsDataAccess(SqlDataReader dataReader, IFileSystem fileSystem)
+        public PrinterJobsDataAccess(SqlDataReader dataReader)
         {
             _dataReader = dataReader;
-            _fileSystem = fileSystem;
+            //_fileSystem = fileSystem;
         }
 
         public IEnumerable<Jobs> GetJobs(IDictionary<string, string> parameters)
         {
             throw new System.NotImplementedException();
             //var result = File.Open("", FileMode.Open);
-            var result = 
-                _fileSystem.File.Open(
-                    "", 
-                    FileMode.Open);
+            //var result = 
+            //    _fileSystem.File.Open(
+            //        "", 
+            //        FileMode.Open);
         }
     }
 }
