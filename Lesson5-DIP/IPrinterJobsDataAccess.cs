1b8a6834278c55e1eecfeb00db83cdf6a2d1bf33
tomasZoulRws <77391310+tomasZoulRws@users.noreply.github.com>
2021-01-13 13:17:51 +0100

    Update IPrinterJobsDataAccess.cs (#2)
    * Update IPrinterJobsDataAccess.cs
    
    * Update Program.cs

--- Lesson5-DIP/IPrinterJobsDataAccess.cs
+++ Lesson5-DIP/IPrinterJobsDataAccess.cs
@@ -1,15 +1,53 @@
-﻿namespace Lesson5_DIP
+using System.Collections.Generic;
+
+namespace Lesson5_DIP
 {
     public interface IPrinterJobsDataAccess
     {
-        SqlDataReader GetJobs(SqlParameterCollection parameters);
-    }
-    
-    public class SqlParameterCollection
-    {
+        IEnumerable<Job> GetJobs(IDictionary<string, string> parameters);
     }
 
-    public class SqlDataReader
+    public class SqlDataReader : ISqlDataReader
     {
+        private readonly string conenctionString;
+
+        public SqlDataReader(string conenctionString)
+        {
+            this.conenctionString = conenctionString;
+        }
+
+        public IEnumerable<Job> Read()
+        {
+            return new List<Job>();
+        }
+    }
+
+    public interface ISqlDataReader
+    {
+        IEnumerable<Job> Read();
+    }
+
+    public class Job
+    {
+        public string Owner { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class PrinterJobsDataAccess : IPrinterJobsDataAccess
+    {
+        private readonly ISqlDataReader sqlDataReader;
+
+        public PrinterJobsDataAccess(ISqlDataReader sqlDataReader)
+        {
+            this.sqlDataReader = sqlDataReader;
+        }
+
+        public IEnumerable<Job> GetJobs(IDictionary<string, string> parameters)
+        {
+            var data = sqlDataReader.Read();
+            return data;
+        }
+
+        //TODO DesignPattern "Strategy"
     }
 }
