7cf901abc0689af94fb7c821750329fd4f4e273f
tomasZoulRws <77391310+tomasZoulRws@users.noreply.github.com>
2021-02-03 12:15:05 +0100

    Update Program.cs

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,76 +1,17 @@
-﻿using System;
-using System.IO;
+using SOLID.InputOutput;
+using SOLID.Util;
 
 namespace SOLID
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Set Command (+. -, *, /");
-            var key = Console.ReadKey();
-
-            switch (key.KeyChar)
-            {
-                case '+':
-                    Add();
-                    break;
-                case '-':
-                    Sub();
-                    break;
-                case '*':
-                    Mul();
-                    break;
-                case '/':
-                    Div();
-                    break;
-                default:
-                    Console.WriteLine("Not supported");
-                    break;
-            }
-        }
-
-        private static void Add()
-        {
-            Console.WriteLine("\nset value 1");
-            var value1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("set value 2");
-            var value2 = int.Parse(Console.ReadLine());
-
-            var result = value1 + value2;
-
-            var output = $"{value1} + {value2} = {result}";
-            Console.WriteLine(output);
-            LogHistory(output);
-        }
-
-        private static void Sub()
-        {
-            Console.WriteLine("\nset value 1");
-            var value1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("set value 2");
-            var value2 = int.Parse(Console.ReadLine());
-
-            var result = value1 - value2;
-
-            var output = $"{value1} - {value2} = {result}";
-            Console.WriteLine(output);
-            LogHistory(output);
-        }
-
-        private static void Mul()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void Div()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void LogHistory(string output)
-        {
-            File.AppendAllText("log.json", $"{DateTime.UtcNow} : {output}\n");
+            Output output = new Output();
+            GetOperand valueGetter = new GetOperand();
+            OperatorFactory operatorFactory = new OperatorFactory();
+            Calc calculator = new Calc(valueGetter, operatorFactory, output);
+            calculator.RunCalculator();
         }
     }
 }
