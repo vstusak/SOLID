1b81fec6f870afd1e1c14e31bb83375f14bc4edb
Daniel.Spacil <daniel.spacil@rws.com>
2021-04-20 09:29:48 +0200

    Added IPerson interface

--- Lesson1-SRP/Program.cs
+++ Lesson1-SRP/Program.cs
@@ -17,9 +17,12 @@ namespace Lesson1_SRP
 
             var salaryProvider = new SalariesProvider();
 
-            var salaries = salaryProvider.GetSalaries();
+            var salaries = salaryProvider.GenerateSalaries(Roles.CEO);
 
-            var retirementSalary = retirementCalculator.Calculate(salaries);
+            var person = new CEO();
+            person.AddSalaries(salaries);
+
+            var retirementSalary = retirementCalculator.Calculate(person);
 
             Console.WriteLine(retirementSalary > 20000
                 ? "Congratulations and have a nice retirement"
@@ -85,18 +88,18 @@ namespace Lesson1_SRP
         }
 
         // TODO Apply CEO base salary - FROM outside object
-        public int Calculate(IEnumerable<Salary> salaries)
+        public int Calculate(IPerson person)
         {
-            var baseSalary = 10000;
+            //var baseSalary = 10000;
             double multiplication = 1;
 
             //salaries.re //ctrl + k, ctrl + c
             
-            multiplication = _rulesProvider.ApplyRulesForMultiplication(salaries, multiplication);
+            multiplication = _rulesProvider.ApplyRulesForMultiplication(person.Salaries, multiplication);
 
-            var bonuses = _rulesProvider.ApplyRulesForBonuses(salaries);
+            var bonuses = _rulesProvider.ApplyRulesForBonuses(person.Salaries);
 
-            var result = Convert.ToInt32(baseSalary * multiplication + bonuses.Sum());
+            var result = Convert.ToInt32(person.BaseSalary * multiplication + bonuses.Sum());
             return result;
         }
     }
@@ -111,22 +114,79 @@ namespace Lesson1_SRP
     {
         public List<Salary> GetSalaries()
         {
-            return JsonSerializer.Deserialize<IEnumerable<Salary>>(File.ReadAllText("salaries.json")).ToList();
+            var result = GenerateSalaries(Roles.Undefined);
+            //return JsonSerializer.Deserialize<IEnumerable<Salary>>(File.ReadAllText("salaries.json")).ToList();
+            return result.ToList();
         }
 
-        public void GenerateSalaries()
+        public IEnumerable<Salary> GenerateSalaries(Roles role)
         {
             var salaryGenerator = new Random();
 
             var salaries = new List<Salary>();
+
+            var min = 5000;
+            var max = 50000;
+            if (role == Roles.CEO)
+            {
+                min = 40000;
+            }
             for (int i = 0; i < 100; i++)
             {
                 salaries.Add(new Salary
-                { DateTime = new DateTime(2020, 9, 13).AddMonths(i * -1), Value = salaryGenerator.Next(5000, 50000) });
+                { DateTime = new DateTime(2020, 9, 13).AddMonths(i * -1), Value = salaryGenerator.Next(min, max) });
             }
 
-            var json = JsonSerializer.Serialize(salaries);
-            File.AppendAllText("salaries.json", json);
+            //var json = JsonSerializer.Serialize(salaries);
+            //File.AppendAllText("salaries.json", json);
+
+            return salaries;
         }
+    }
+
+    public enum Roles
+    {
+        Undefined,
+        Worker,
+        Manager,
+        CEO
+    }
+
+    public interface IPerson
+    {
+        int BaseSalary { get; }
+        IEnumerable<Salary> Salaries { get; }
+
+        void AddSalary(Salary salary);
+        void AddSalaries(IEnumerable<Salary> salaries);
+    }
+
+    public abstract class Person : IPerson
+    {
+        private List<Salary> _salaries = new List<Salary>();
+
+        public abstract int BaseSalary { get; }
+
+        public IEnumerable<Salary> Salaries { get; }
+
+        public void AddSalaries(IEnumerable<Salary> salaries)
+        {
+            _salaries.AddRange(salaries);
+        }
+
+        public void AddSalary(Salary salary)
+        {
+            _salaries.Add(salary);
+        }
+    }
+
+    public class Worker : Person, IPerson
+    {
+        public override int BaseSalary => 20000;
+    }
+
+    public class CEO : Person, IPerson
+    {
+        public override int BaseSalary => 40000;
     }
 }
