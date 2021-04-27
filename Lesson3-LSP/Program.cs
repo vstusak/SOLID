3151e822d421555f270df878e2a026b72a2fbefa
MartinaSmahelova <martina.smahelova@rws.com>
2021-04-27 09:30:18 +0200

    lesson3 LOS

--- Lesson3-LSP/Program.cs
+++ Lesson3-LSP/Program.cs
@@ -7,23 +7,52 @@ namespace Lesson3_LSP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var square = new Square();
+            Rectangle myRect = new Rectangle();
+            myRect.Height = 10;
+            myRect.Width = 15;
+
+            var calculator = new Calculator();
+            var result = calculator.CalculateArea(myRect);
+            Console.WriteLine(result);
+
+            square.Width = 12;
+            square.Height = 8;
+            var resultSquare = calculator.CalculateArea(square);
+            Console.WriteLine(resultSquare);
+
+            Console.ReadLine();
         }
 
         public class Rectangle
         {
-            
+            public virtual int Width { get; set; }
+            public virtual int Height { get; set; }
         }
 
-        public class Square
+        public class Square : Rectangle
         {
+            private int _width;
+            private int _height;
+
+            public override int Width
+            {
+                get { return _width; }
+                set { _width = _height = value; }
+            }
+            public override int Height
+            {
+                get { return _height; }
+                set { _height = _width = value; }
+            }
 
         }
 
         public class Calculator
         {
-            public int CalculateArea()
+            public int CalculateArea(Rectangle myRect)
             {
-                return 0;
+                return myRect.Height * myRect.Width;
             }
         }
     }
