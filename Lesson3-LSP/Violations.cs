0a6c2a46930ee1404a742ed0c8f8306bea532c18
JKafka <Jaroslav.Kafka@rws.com>
2021-05-11 09:27:14 +0200

    LSP 11/5/2021

--- Lesson3-LSP/Violations.cs
+++ Lesson3-LSP/Violations.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 
 namespace Lesson3_LSP
 {
+    public static class Extensions
+    {
+        public static string GetDescription(this Person person, int count)
+        {
+            return person switch
+            {
+                Male _ => "It's male",
+                Female _ => "It's female",
+                Child _ => "It's child",
+                Turtle _ => "It's turtle",
+                _ => "It's undefined"
+            };
+        }
+    }
     public class Violations
     {
         public void DoSomething()
@@ -11,44 +25,57 @@ namespace Lesson3_LSP
 
             foreach (Person person in people)
             {
-                if (person is Male)
-                {
-                    Console.WriteLine("It's male");
-                }
-                else if (person is Female)
-                {
-                    Console.WriteLine("It's female");
-                }
-                else
-                {
-                    Console.WriteLine("It's undefined");
-                }
+                //Console.WriteLine(person.GetDescription(5));
+                Console.WriteLine(person.GetText());
             }
-
         }
 
         private IEnumerable<Person> GeneratePeople()
         {
             return new List<Person>
             {
-                new Person(), new Female(), new Male(), new Person(), new Female(), new Male()
+                new Person(), new Female(), new Male(), new Person(), new Female(), new Male(), new Child(), new Turtle()
             };
         }
     }
 
     public class Person
     {
-
+        public virtual string GetText()
+        {
+            return "It's undefined";
+        }
     }
 
     public class Male : Person
     {
-
+        public override string GetText()
+        {
+            return "It's male";
+        }
     }
 
     public class Female : Person
     {
+        public override string GetText()
+        {
+            return "It's female";
+        }
+    }
 
+    public class Child : Person
+    {
+        public override string GetText()
+        {
+            return "It's child";
+        }
+    }
+
+    public class Turtle : Person
+    {
+        public override string GetText()
+        {
+            return "It's turtle";
+        }
     }
 }
-
