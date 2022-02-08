192448da73e44c443bb9c082f99fa4b8043833d5
Samuel Zigo <SZigo@moravia.com>
2022-02-08 14:02:06 +0100

    first Unit test

---
+++ Lesson1-SRP.Tests/MultiplicationProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson1_SRP.Tests
+{
+    public class MultiplicationProviderTests
+    {
+        public void ThreeSalariesLessThan30000DefaultMultiplcation01_ApplyRules_FinalMultiplication01()
+        {
+            //Arrange
+            MultiplicationProvider underTest = new MultiplicationProvider();
+            double defaultMultiplication = 0.1;
+            var listOfSalaries = new List<Salary> {   
+                new Salary{ DateTime = DateTime.Now, Value = 25000 },
+                new Salary{ DateTime = DateTime.Now, Value = 25000 },
+                new Salary{ DateTime = DateTime.Now, Value = 25000 }
+            };
+            //Act
+            var finalMultiplication = underTest.ApplyRules(defaultMultiplication,listOfSalaries);
+            //Assert
+            Assert.
+        }
+
+    }
+}
