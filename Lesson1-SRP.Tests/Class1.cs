61ad16b5ac55a339515753f22e79f5b2df8b5291
Petr Jankovsky <petr.jankovsky@rws.com>
2022-02-09 12:00:09 +0100

    WorkInProgress-UnitTests

---
+++ Lesson1-SRP.Tests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_SRP.Tests
+{
+    public class MultiplicationRulesProviderTests
+    {
+        public void ApplyRules_3SalariesSalariesAverage20kMultiplication01_ExpectedMultiplication01()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
