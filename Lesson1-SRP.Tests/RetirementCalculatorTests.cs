b65b505e8d207778829b1e74c7aa2c6826c6be75
Tomáš Fulajtár <tomasfu@moravia.com>
2022-03-09 12:00:52 +0100

    RetirementCalculator Mock.

--- Lesson1-SRP.Tests/RetirementCalculatorTests.cs
+++ Lesson1-SRP.Tests/RetirementCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,29 @@ namespace Lesson1_SRP.Tests
         [Fact]
         public void RetirementCalculator()
         {
+            //Arrange
+
+            var multiplicationRulesProviderMock = new Mock<IMultiplicationRulesProvider>(MockBehavior.Strict);
+            var bonusesRulesProviderMock = new Mock<IBonusesRulesProvider>(MockBehavior.Strict);
+
+            //multiplicationRulesProviderMock.Setup(mrpm => mrpm.ApplyRules( new List<Salary>(), 0)).Returns(1);
+            //multiplicationRulesProviderMock.Setup(mrpm => mrpm.ApplyRules(null, 0)).Returns(1);
+            multiplicationRulesProviderMock.Setup(mrpm => mrpm.ApplyRules(It.IsAny<IEnumerable<Salary>>(), It.IsAny<double>())).Returns(1);
+            //bonusesRulesProviderMock.Setup(brpm => brpm.ApplyRules(null)).Returns(new List<int>());
+
+            bonusesRulesProviderMock.Setup(brpm => brpm.ApplyRules(It.IsAny<IEnumerable<Salary>>())).Returns(new List<int>{ 1, 1, 1 });
+            var retirementCalculator = new RetirementCalculator(multiplicationRulesProviderMock.Object,bonusesRulesProviderMock.Object);
+            var expectedResult = 10003;
+            IEnumerable<Salary> salaries = new List<Salary>();
+            IEmployee employee = new Employee();
+
+            //Act
+            var actualResult = retirementCalculator.Process(salaries, employee);
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+
+            //Todo: add check for howmany times the method has been called.
+            
 
         }
     }
