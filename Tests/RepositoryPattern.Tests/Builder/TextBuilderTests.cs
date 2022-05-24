721ec5457fc3d46eb5dc4c259ce3b47c1da79577
Dan Spacil <Daniel.Spacil@rws.com>
2022-05-24 09:45:26 +0200

    Added TextBuilder tests

---
+++ Tests/RepositoryPattern.Tests/Builder/TextBuilderTests.cs
@@ -0,0 +1,91 @@
+﻿using BuilderPattern;
+using BuilderPattern.Builder;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace RepositoryPattern.Tests.Builder
+{
+    [TestFixture]
+    public class TextBuilderTests : TestsBase
+    {
+        private Mock<IEnumerable<Book>> mockEnumerable;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockEnumerable = MockRepository.Create<IEnumerable<Book>>();
+        }
+
+        private TextBuilder CreateTextBuilder()
+        {
+            return new TextBuilder(
+                mockEnumerable.Object);
+        }
+
+        [Test]
+        public void SetHeader_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var textBuilder = CreateTextBuilder();
+
+            // Act
+            var result = textBuilder.SetHeader();
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void WriteLibrary_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var textBuilder = CreateTextBuilder();
+
+            // Act
+            var result = textBuilder.WriteLibrary();
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void SetFooter_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var textBuilder = CreateTextBuilder();
+
+            // Act
+            var result = textBuilder.SetFooter();
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void AddDateStamp_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var textBuilder = CreateTextBuilder();
+
+            // Act
+            var result = textBuilder.AddDateStamp();
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Build_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var textBuilder = CreateTextBuilder();
+
+            // Act
+            var result = textBuilder.Build();
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
