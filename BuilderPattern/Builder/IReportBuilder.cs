9f2ff3a5c88e37de84ec05454c90fbf87f7a060e
Vladimir Stusak <vstusak@users.noreply.github.com>
2022-05-24 09:34:45 +0200

    adding builder project for testing

---
+++ BuilderPattern/Builder/IReportBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace BuilderPattern.Builder
+{
+    public interface IReportBuilder
+    {
+        IReportBuilder SetHeader();
+
+        IReportBuilder WriteLibrary();
+
+        IReportBuilder SetFooter();
+
+        IReportBuilder AddDateStamp();
+
+        string Build();
+    }
+}
---
+++ BuilderPattern/Builder/TextBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BuilderPattern.Builder
+{
+    public class TextBuilder : IReportBuilder
+    {
+        private readonly IEnumerable _books;
+        internal readonly StringBuilder _result = new();
+
+        public TextBuilder(IEnumerable<Book> books)
+        {
+            _books = books;
+        }
+
+        public IReportBuilder SetHeader()
+        {
+            _result.AppendLine("Welcome to our library, there is a list of books.");
+            return this;
+        }
+
+        public IReportBuilder WriteLibrary()
+        {
+            foreach (var book in _books)
+            {
+                _result.AppendLine(book.ToString());
+            }
+            return this;
+        }
+
+        public IReportBuilder SetFooter()
+        {
+            _result.AppendLine("Thanks for reading.");
+            return this;
+        }
+
+        public IReportBuilder AddDateStamp()
+        {
+            _result.AppendLine(DateTime.UtcNow.ToString(CultureInfo.InvariantCulture));
+            return this;
+        }
+
+        public string Build()
+        {
+            return _result.ToString();
+        }
+    }
+}
