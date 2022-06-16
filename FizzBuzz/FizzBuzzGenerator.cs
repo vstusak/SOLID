a89b7ec7b04443ab9e9958fbffa7a5c4827daf82
Michaela Vitula <MVitula@moravia.com>
2022-06-16 10:04:47 +0200

    add FizzBuzz generator

---
+++ FizzBuzz/FizzBuzzGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzGenerator
+    {
+        public string GetSequenceOfTwenty(string input)
+        {
+            string result = string.Empty;
+            for (int i = Int32.Parse(input); i < 20 + Int32.Parse(input); i++)
+            {
+                if (i % 3 == 0)
+                {
+                    result = result + "fizz";
+                }
+                else
+                {
+                    result = result + i.ToString();
+                }
+            }
+            return result;
+        }
+    }
+}
