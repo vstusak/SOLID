9f43eddc4869fd43faf1b3221ad1d697eb611c5e
Filip Mucha <filip.mucha@rws.com>
2022-06-28 09:57:22 +0200

    added fizzbuzz with parsing

--- FizzBuzz.Tests/UnitTest1.cs
+++ FizzBuzz.Tests/UnitTest1.cs
@@ -19,7 +19,23 @@ namespace FizzBuzz.Tests
             //Act
             FizzBuzzGenerator fizzBuzzGenerator = new FizzBuzzGenerator();
             var result = fizzBuzzGenerator.GetSequenceOfTwenty(input);
-            
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            //Arrange
+            string input = "1";
+            string iterationCount = "22";
+            string expectedResult = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz fizz 22";
+
+            //Act
+            FizzBuzzGenerator fizzBuzzGenerator = new FizzBuzzGenerator();
+            var result = fizzBuzzGenerator.GetSequence(input, iterationCount);
+
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
--- FizzBuzz/FizzBuzzGenerator.cs
+++ FizzBuzz/FizzBuzzGenerator.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FizzBuzz
 {
@@ -8,19 +6,62 @@ namespace FizzBuzz
     {
         public string GetSequenceOfTwenty(string input)
         {
+            return GetSequence(input, "20");
+        }
+
+
+        public string GetSequence(string input, string iterationCount)
+        {
             string result = string.Empty;
-            for (int i = Int32.Parse(input); i < 20 + Int32.Parse(input); i++)
+            for (int i = Int32.Parse(input); i < Int32.Parse(iterationCount) + Int32.Parse(input); i++)
             {
-                if (i % 3 == 0)
+                if (i % 3 == 0 && i % 5 == 0)
                 {
-                    result = result + "fizz";
+                    result = result + "fizzbuzz ";
+                }
+                else if (i % 3 == 0)
+                {
+                    result = result + "fizz ";
+                }
+                else if (i % 5 == 0)
+                {
+                    result = result + "buzz ";
                 }
                 else
                 {
-                    result = result + i.ToString();
+                    result = result + i.ToString() + " ";
                 }
             }
-            return result;
+            return result.Trim();
+        }
+        public string GetSequenceWithParsing(string input, string iterationCount)
+        {
+            string result = string.Empty;
+            var parsedInput = Int32.Parse(input);
+            var parsedIterationCount = Int32.Parse(iterationCount);
+
+            for (int i = parsedInput; i < parsedIterationCount + parsedInput; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result = result + "fizzbuzz ";
+                }
+                else if (i % 3 == 0)
+                {
+                    result = result + "fizz ";
+                }
+                else if (i % 5 == 0)
+                {
+                    result = result + "buzz ";
+                }
+                else
+                {
+                    result = result + i.ToString() + " ";
+                }
+            }
+            return result.Trim();
         }
     }
 }
+
+
