4c7f5e9a4636869898b9efec69cd2dca332411c9
Marek Vesely <MVesely@moravia.com>
2023-02-17 11:43:16 +0100

    4-LSP, Square, Rectangle -> Shape

--- Lesson3-LSP/Program.cs
+++ Lesson3-LSP/Program.cs
@@ -6,24 +6,40 @@ namespace Lesson3_LSP
     {
         static void Main(string[] args)
         {
+            var square = new Square();
+            square.
             Console.WriteLine("Hello World!");
         }
 
-        public class Rectangle
+        public abstract class Shape
         {
-            
+            public abstract int GetArea();
+        }
+        public class Rectangle : Shape
+        {
+            public int a { get; set; }
+            public int b { get; set; }
+            public override int GetArea()
+            {
+                return a * b;
+            }
         }
 
-        public class Square
+        public class Square : Shape
         {
+            public int a { get; set; }
 
+            public override int GetArea()
+            {
+                return a * a;
+            }
         }
 
         public class Calculator
         {
-            public int CalculateArea()
+           public int CalculateArea(Shape s)
             {
-                return 0;
+                return s.GetArea();
             }
         }
     }
