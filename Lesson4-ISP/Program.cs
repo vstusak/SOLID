b486e7d3e446ab108014aa0df24691b11cff6ce1
Marek Vesely <MVesely@moravia.com>
2023-03-10 10:01:22 +0100

    Split interfaces

--- Lesson4-ISP/Program.cs
+++ Lesson4-ISP/Program.cs
@@ -19,4 +19,27 @@ namespace Lesson4_ISP
         void SendPostcard(string name, string street, string town, string message);
         void ShowPopup(string clientUri, string message);
     }
+
+    public interface IEmailNotificationService
+    {
+        void SendEmail(string address, string message);
+        void SendEmail(IEnumerable<string> addresses, string message);
+    }
+    public interface ITextNotificationService
+    {
+        void SendText(string phoneNumber, string message);
+        }
+    public interface IPostcardNotificationService
+    {
+        void SendPostcard(string name, string street, string town, string message);
+    }
+
+    public interface IPopupNotificationService
+    {
+        void ShowPopup(string clientUri, string message);
+    }
+
+    Dedicnost interfaců
+        Adapter design pattern
+
 }
