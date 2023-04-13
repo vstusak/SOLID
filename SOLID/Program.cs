ec9fb0dee481d2714938ed286c3df3298c2c6c97
Michaela Vitula <MVitula@moravia.com>
2023-04-13 22:44:36 +0200

    added fileds

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
 
 namespace SOLID
 {
     internal class Program
     {
+        public static int Value1;
+        public static int Value2;
+        public static ConsoleKeyInfo OperatorKey;
+        public static string Output;
+
         static void Main(string[] args)
         {
+            //TODO: pass values
             var calculator = new Calculator();
 
             calculator.GetValues();
             calculator.GetOperator();
-            calculator.Count();
-            calculator.LogHistory(output);
+            calculator.Count(OperatorKey, Value1, Value2);
+            calculator.LogHistory(Output);
         }
     }
 }
