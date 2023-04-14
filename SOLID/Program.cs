8bd51fb7c80cda686bbb91111f541cb8c003fe46
Marek Vesely <MVesely@moravia.com>
2023-04-14 03:31:26 +0200

    notpc

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,79 +1,159 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SOLID
 {
+    //call new object Calculator -> method Calculate
+    //method calculate should return Result
+    //5-7 rows
+    //only Main in class Program
     class Program
     {
-        static void Main(string[] args)
+        void Main(string[] args)
         {
-            Console.WriteLine("Set Command (+. -, *, /");
-            var key = Console.ReadKey();
-            //call new object Calculator -> method Calculated
-            //method calculate should return Result
-            //5-7 rows
-            //only Main in class Program
-            switch (key.KeyChar)
+            var value1 = ValueGetter1();
+            var value2 = ValueGetter2();
+            var calculator = new Calculator(CalculationMethodSelector(InputCalculationMethod(out ConsoleKeyInfo key)));
+            calculator.Calculate(value1, value2);
+        }
+
+        public ICalculatorMethodProvider CalculationMethodSelector(char keychar)
+        {
+       
+            switch (keychar)
             {
+               
                 case '+':
-                    Add();
-                    break;
+                    var add = new CalculatorMethodProviderAdd();
+                    return add;
                 case '-':
-                    Sub();
-                    break;
+                    var sub = new CalculatorMethodProviderSub();
+                    return sub;
                 case '*':
-                    Mul();
-                    break;
+                    var mul = new CalculatorMethodProviderDiv();
+                    return mul;
                 case '/':
-                    Div();
-                    break;
+                    var div = new CalculatorMethodProviderDiv();
+                    return div;
                 default:
                     Console.WriteLine("Not supported");
-                    break;
+                    return null;
             }
         }
 
-        private static void Add()
+       private static char InputCalculationMethod(out ConsoleKeyInfo key)
+        {
+            Console.WriteLine("Set Command (+. -, *, /");
+            key = Console.ReadKey();
+            return key.KeyChar;
+        }
+
+
+        public interface ICalculatorMethodProvider
+        {
+            string Process(int value1, int value2);
+        }
+
+        public class CalculatorMethodProviderAdd : ICalculatorMethodProvider
+        {
+            public string Process(int value1, int value2)
+            {
+                var result = value1 + value2;
+                var resultMessage = $"{value1} + {value2} = {result}";
+                return resultMessage;
+            }
+        }
+        public class CalculatorMethodProviderSub : ICalculatorMethodProvider
+        {
+            public string Process(int value1, int value2)
+            {
+                var result = value1 - value2;
+                var resultMessage = $"{value1} - {value2} = {result}";
+                return resultMessage;
+            }
+        }
+        public class CalculatorMethodProviderMul : ICalculatorMethodProvider
+        {
+            public string Process(int value1, int value2)
+            {
+                var result = value1 * value2;
+                var resultMessage = $"{value1} * {value2} = {result}";
+                return resultMessage;
+            }
+        }
+        public class CalculatorMethodProviderDiv : ICalculatorMethodProvider
+        {
+            public string Process(int value1, int value2)
+            {
+                var result = value1 / value2;
+                var resultMessage = $"{value1} / {value2} = {result}";
+                return resultMessage;
+            }
+        }
+
+        public class Calculator
+        {
+            private readonly ICalculatorMethodProvider _calculatorMethodProvider;
+            public Calculator(ICalculatorMethodProvider calculatorMethodProvider)
+            {
+                _calculatorMethodProvider = calculatorMethodProvider;
+            }
+
+            public void Calculate(int value1, int value2)
+            {
+                _calculatorMethodProvider.Process(value1, value2);
+            }
+        }
+        //public class RetirementCalculator
+        //{
+        //    private readonly IRetirementRulesProvider _rulesProvider;
+        //    public RetirementCalculator(IRetirementRulesProvider rulesProvider)
+        //    {
+        //        _rulesProvider = rulesProvider;
+        //    }
+
+        //    public int Process(List<Salary> salaries, int baseRetirementSalary)
+        //    {
+        //        var multiplication = _rulesProvider.GetMultiplication(salaries);
+        //        var bonusSum = _rulesProvider.GetBonuses(salaries);
+
+        //        return Convert.ToInt32(baseRetirementSalary * multiplication + bonusSum);
+        //    }
+        //}
+        public class ResultsManager
+        {
+                private readonly ICalculatorMethodProvider _calculatorMethodProvider;
+            public ResultsManager(ICalculatorMethodProvider calculatorMethodProvider)
+            {
+                _calculatorMethodProvider = calculatorMethodProvider;
+            }
+
+            public void WriteResult(string output)
+            {
+                Console.WriteLine(output);
+            }
+            public void LogHistory(string _output)
+            {
+                File.AppendAllText("salaries.json", $"{DateTime.UtcNow} : {_output}\n");
+            }
+        }
+  
+        private static int ValueGetter2()
+        {
+            Console.WriteLine("set value 2");
+            var value2 = int.Parse(Console.ReadLine());
+            return value2;
+        }
+
+        private static int ValueGetter1()
         {
             Console.WriteLine("\nset value 1");
             var value1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("set value 2");
-            var value2 = int.Parse(Console.ReadLine());
-
-            var result = value1 + value2;
-
-            var output = $"{value1} + {value2} = {result}";
-            Console.WriteLine(output);
-            LogHistory(output);
+            return value1;
         }
 
-        private static void Sub()
-        {
-            Console.WriteLine("\nset value 1");
-            var value1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("set value 2");
-            var value2 = int.Parse(Console.ReadLine());
-
-            var result = value1 - value2;
-
-            var output = $"{value1} - {value2} = {result}";
-            Console.WriteLine(output);
-            LogHistory(output);
-        }
-
-        private static void Mul()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void Div()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void LogHistory(string output)
-        {
-            File.AppendAllText("log.json", $"{DateTime.UtcNow} : {output}\n");
-        }
     }
 }
