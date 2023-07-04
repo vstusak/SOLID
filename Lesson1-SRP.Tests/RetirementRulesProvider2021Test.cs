7fa9414dd05e476bc4fcc7b9135ef76bb47dfa2b
Tereza Nekutova <TNekutova@moravia.com>
2023-07-04 10:08:10 +0200

    Test project iniciated

--- Lesson1-SRP.Tests/RetirementRulesProvider2021Test.cs
+++ Lesson1-SRP.Tests/RetirementRulesProvider2021Test.cs
@@ -2,6 +2,15 @@
 {
     public class RetirementRulesProvider2021Test
     {
+        public void GetBonuses_SalaryOver50000_ExpectBonus2000()
+        {
+            //arange
+            var rulesProvider = new RetirementRulesProvider2021();
+            var salaries = new List<Salary>();
+            salaries.Add(new Salary());
+            //act
+            //assert
+        }
 
     }
 }
