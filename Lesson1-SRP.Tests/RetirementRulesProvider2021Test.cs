1f5cca9b94ba141964f9185caa987b2315bc9ba4
Petr Kosina <PKosina@moravia.com>
2023-07-11 09:58:05 +0200

    test continue

--- Lesson1-SRP.Tests/RetirementRulesProvider2021Test.cs
+++ Lesson1-SRP.Tests/RetirementRulesProvider2021Test.cs
@@ -1,15 +1,20 @@
-﻿namespace Lesson1_SRP.Tests
+﻿using NUnit.Framework;
+
+namespace Lesson1_SRP.Tests
 {
     public class RetirementRulesProvider2021Test
     {
+        [Test]
         public void GetBonuses_SalaryOver50000_ExpectBonus2000()
         {
             //arange
             var rulesProvider = new RetirementRulesProvider2021();
             var salaries = new List<Salary>();
-            salaries.Add(new Salary());
+            salaries.Add(new Salary(){Value = 55000});
             //act
+            var result = rulesProvider.GetBonuses(salaries);
             //assert
+            Assert.AreEqual(2000, result);
         }
 
     }
