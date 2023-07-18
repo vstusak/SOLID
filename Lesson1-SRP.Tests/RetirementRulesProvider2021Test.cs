1e0807d6a88f2a047574ab1326e93fc520227aa2
Marek Vesely <MVesely@moravia.com>
2023-07-18 10:00:40 +0200

    UnitTest continue, added test cases, test categories

--- Lesson1-SRP.Tests/RetirementRulesProvider2021Test.cs
+++ Lesson1-SRP.Tests/RetirementRulesProvider2021Test.cs
@@ -10,11 +10,41 @@ namespace Lesson1_SRP.Tests
             //arange
             var rulesProvider = new RetirementRulesProvider2021();
             var salaries = new List<Salary>();
-            salaries.Add(new Salary(){Value = 55000});
+            salaries.Add(new Salary() { Value = 55000 });
             //act
             var result = rulesProvider.GetBonuses(salaries);
             //assert
             Assert.AreEqual(2000, result);
+        }
+
+        [Test]
+        public void GetBonuses_SalaryUnder45000_ExpectNoBonus()
+        {
+            //arange
+            var rulesProvider = new RetirementRulesProvider2021();
+            var salaries = new List<Salary>();
+            salaries.Add(new Salary(){Value = 44000});
+            //act
+            var result = rulesProvider.GetBonuses(salaries);
+            //assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestCase(47000, 0)]//SalaryOver47000_ExpectedBonus2000
+        [TestCase(47001, 2000, Category=TestCategories.Positive)]
+        [TestCase(0, 0, Category= TestCategories.Negative)]
+        [Category(TestCategories.UnitTest)]
+
+        public void GetBonuses_Salary_ExpectedBonus(int salary, int expectedBonus)
+        {
+            //arange
+            var rulesProvider = new RetirementRulesProvider2021();
+            var salaries = new List<Salary>();
+            salaries.Add(new Salary(){Value = salary});
+            //act
+            var actual = rulesProvider.GetBonuses(salaries);
+            //assert
+            Assert.AreEqual(expectedBonus, actual);
         }
 
     }
---
+++ Lesson1-SRP.Tests/TestCategories.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_SRP.Tests
+{
+    public static class TestCategories
+    {
+        public const string UnitTest = "UnitTest";
+        public const string Positive = "Positive";
+        public const string Negative = "Negative";
+        public const string LongRunning = "LongRunning";
+        public const string Short = "Short";
+    }
+}
