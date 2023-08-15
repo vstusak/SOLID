f0f5bdb803070b2b377d874c87ae6dbd0ef2f084
Marek Vesely <MVesely@moravia.com>
2023-08-15 08:48:05 +0200

    ExpectedException Test

--- Lesson1-SRP.Tests/RetirementCalculatorTest.cs
+++ Lesson1-SRP.Tests/RetirementCalculatorTest.cs
@@ -6,13 +6,11 @@ namespace Lesson1_SRP.Tests
     public class RetirementCalculatorTest
     {
         [TestCase(10000, 2, 3, 20003)]
-        [TestCase(10000, 0, 0, 0)]
-        [TestCase(10000, -1, -1, -10001)]
+        [TestCase(10000, 0.001, 0, 10)]
         [TestCase(0, 2, 3, 3)]
-        [TestCase(0, -1, -1, -1)]
-        [TestCase(-1, -1, -1, 0)]
-        [TestCase(-1, -1, 1, 2)]
-        public void POC_Process_MockingTest(int baseRetirementSalary, int multiplication, int bonuses, int expectedResult)
+        [TestCase(-10000, 2, 3, 3)]
+
+        public void Proces_ValidInputs_ExpectedOutputs(int baseRetirementSalary, double multiplication, int bonuses, int expectedResult)
         {
             //arrange
             //var retirementRulesProvider2021 = new RetirementRulesProvider2021();
@@ -28,7 +26,30 @@ namespace Lesson1_SRP.Tests
             //assert
             Assert.AreEqual(expectedResult, actualResult, $"CHECK: Calculation of retirement failed:");
         }
-        //TODO continue: CleanUp test to make sense (negative values)
+        
+        [Test]
+        public void Process_NegativeBaseSalary_ExpectedExceptionThrown()
+        {
+            //arrange
+            var baseRetirementSalary = -5;
+            //var retirementRulesProvider2021 = new RetirementRulesProvider2021();
+            var retirementRulesProviderMock = new Mock<IRetirementRulesProvider>(MockBehavior.Strict);
+            retirementRulesProviderMock.Setup(rrp => rrp.GetMultiplication(It.IsAny<List<Salary>>())).Returns(2);
+            retirementRulesProviderMock.Setup(rrp => rrp.GetBonuses(It.IsAny<List<Salary>>())).Returns(3);
+            var retirementCalculator = new RetirementCalculator(retirementRulesProviderMock.Object);
+            var salaries = new List<Salary>();
+            
+            //act
+            Assert.Throws<Exception>(() => retirementCalculator.Process(salaries, baseRetirementSalary),"Expected exception was not thrown.");
+            //var actualResult = retirementCalculator.Process(salaries, baseRetirementSalary);
+
+            //assert
+            //Assert.AreEqual(expectedResult, actualResult, $"CHECK: Calculation of retirement failed:");
+        }
+        //TODO continue: finish exception test, finish exception implementation
+        //TODO create Expected Exception Attribute
+        //TODO create Mock Repository
+        //TODO use boiler Template Test
 
     }
 }
