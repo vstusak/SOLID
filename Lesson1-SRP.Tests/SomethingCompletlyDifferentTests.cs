afef4f80b2ad4a60eb7668db20559b9bee2557ca
Petr Kosina <PKosina@moravia.com>
2023-10-17 10:01:49 +0200

    New Class

---
+++ Lesson1-SRP.Tests/SomethingCompletlyDifferentTests.cs
@@ -0,0 +1,43 @@
+﻿using Lesson1_SRP;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Lesson1_SRP.Tests
+{
+    [TestFixture]
+    public class SomethingCompletlyDifferentTests: TestBase
+    {
+        private Mock<IRetirementRulesProvider> mockRetirementRulesProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockRetirementRulesProvider = mockRepository.Create<IRetirementRulesProvider>();
+        }
+
+        private SomethingCompletlyDifferent CreateSomethingCompletlyDifferent()
+        {
+            return new SomethingCompletlyDifferent(
+                this.mockRetirementRulesProvider.Object);
+        }
+
+        [Test]
+        public void Process_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var somethingCompletlyDifferent = this.CreateSomethingCompletlyDifferent();
+            List salaries = null;
+            int baseRetirementSalary = 0;
+
+            // Act
+            var result = somethingCompletlyDifferent.Process(
+                salaries,
+                baseRetirementSalary);
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
---
+++ Lesson1-SRP.Tests/TestBase.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_SRP.Tests
+// ToDo: What is Abstract
+
+{
+    public class TestBase
+    {
+        protected MockRepository mockRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            mockRepository.VerifyAll();
+        }
+    }
+}
