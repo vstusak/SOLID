c48b7d486c4203b3daef4a856efc481f78bfa33f
Linda Adamkova <LAdamkova@moravia.com>
2024-05-27 09:48:15 +0200

    RetirementCalculator refactoring // TODO refactor main

--- Lesson1-SRP/Program.cs
+++ Lesson1-SRP/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
 
 namespace Lesson1_SRP
 {
@@ -14,7 +10,7 @@ namespace Lesson1_SRP
             var retirementCalculator = new RetirementCalculator();
 
             //retirementCalculator.GenerateSalaries();
-            var retirementSalary = retirementCalculator.Process();
+            var retirementSalary = retirementCalculator.Process(10000);
 
             Console.WriteLine(retirementSalary > 20000
                 ? "Congratulations and have a nice retirement"
@@ -22,50 +18,6 @@ namespace Lesson1_SRP
         }
     }
     //TODO - premistit tridu do jineho souboru
-    public class RetirementCalculator
-    {
-        public int Process()
-        {
-            var baseSalary = 10000;
-            double multiplication = 1;
-            var bonuses = new List<int>();
-
-            var salaries = JsonSerializer.Deserialize<IEnumerable<Salary>>(File.ReadAllText("salaries.json")).ToList();
-
-            if (salaries.Count() > 50)
-            {
-                multiplication += 0.3;
-            }
-
-            if (salaries.Select(salary => salary.Value).Average() > 30000)
-            {
-                multiplication += 1;
-            }
-
-            if (salaries.Select(salary => salary.Value).Any(value => value > 47000))
-            {
-                bonuses.Add(2000);
-            }
-
-            return Convert.ToInt32(baseSalary * multiplication + bonuses.Sum());
-        }
-
-        //TODO - getSalaryConditions? method - presunout ify
-        public void GenerateSalaries()
-        {
-            var salaryGenerator = new Random();
-
-            var salaries = new List<Salary>();
-            for (int i = 0; i < 100; i++)
-            {
-                salaries.Add(new Salary
-                    { DateTime = new DateTime(2020, 9, 13).AddMonths(i * -1), Value = salaryGenerator.Next(5000, 50000) });
-            }
-
-            var json = JsonSerializer.Serialize(salaries);
-            File.AppendAllText("salaries.json", json);
-        }
-    }
 
     public class Salary
     {
---
+++ Lesson1-SRP/RetirementCalculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace Lesson1_SRP;
+
+public class RetirementCalculator
+{
+    public int Process(int baseSalary)
+    {
+        var salaries = GetSalaries();
+        
+        var multiplication = GetMultiplication(salaries);
+
+        var bonuses = GetBonuses(salaries);
+
+        return Convert.ToInt32(baseSalary * multiplication + bonuses.Sum());
+    }
+
+    private List<Salary> GetSalaries()
+    {
+        return JsonSerializer.Deserialize<IEnumerable<Salary>>(File.ReadAllText("salaries.json")).ToList();
+    }
+
+    private double GetMultiplication(List<Salary> salaries)
+    {
+        double multiplication = 1;
+        if (salaries.Count() > 50)
+        {
+            multiplication += 0.3;
+        }
+
+        if (salaries.Select(salary => salary.Value).Average() > 30000)
+        {
+            multiplication += 1;
+        }
+
+        return multiplication;
+    }
+
+    private List<int> GetBonuses(List<Salary> salaries)
+    {
+        var bonuses = new List<int>();
+
+        if (salaries.Select(salary => salary.Value).Any(value => value > 47000))
+        {
+            bonuses.Add(2000);
+        }
+
+        return bonuses;
+    }
+
+    //TODO - getSalaryConditions? method - presunout ify
+    public void GenerateSalaries()
+    {
+        var salaryGenerator = new Random();
+
+        var salaries = new List<Salary>();
+        for (int i = 0; i < 100; i++)
+        {
+            salaries.Add(new Salary
+                { DateTime = new DateTime(2020, 9, 13).AddMonths(i * -1), Value = salaryGenerator.Next(5000, 50000) });
+        }
+
+        var json = JsonSerializer.Serialize(salaries);
+        File.AppendAllText("salaries.json", json);
+    }
+}
