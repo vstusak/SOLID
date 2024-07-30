d4129d895c92da7dcf2eabce1c3edc215c8c4077
Jiri Pazdera <JPazdera@moravia.com>
2024-07-30 10:00:22 +0200

    Refactoring validator class created

--- Lesson1-SRP/Program.cs
+++ Lesson1-SRP/Program.cs
@@ -15,30 +15,28 @@ namespace Lesson1_SRP
         //  As a logged-in user I need to be able to call api endpoint to get calculation whether I will need to work during retirement.
         static void Main(string[] args)
         {
-            if (args.Length != 1)
-            {
-                throw new ArgumentException("Application started with wrong number of parameters.");
-            }
-            var salarySourcePath = args.First();
+            //if (args.Length != 1)
+            //{
+            //    throw new ArgumentException("Application started with wrong number of parameters.");
+            //}
+            //if (!File.Exists(salarySourcePath))
+            //{
+            //    throw new ArgumentException("File doesnt exist.");
+            //}
 
-            if (!File.Exists(salarySourcePath))
-            {
-                throw new ArgumentException("File doesnt exist.");
-            }
-            
-            // TODO validation method for args - 1) file exists, 2) file is in json format (maybe not)
-            // TODO extract method to separate class
+            // DONE validation method for args - 1) file exists, 2) file is in json format (maybe not)
+            // DONE extract method to separate class
             // TODO app run based on user input (select salaries source file while application is running)
+            // TODO revert finished DONEs and implement from usage point of view
 
-
-            IApplicationInputValidator applicationInputValidator = new ApplicationInputValidator();
-            var isApplicationInputValid = applicationInputValidator.Validate(args);
+            var applicationInputValidator = new ApplicationInputValidator();
+            var isApplicationInputValid = applicationInputValidator.ValidateInputArguments(args);
             if (!isApplicationInputValid)
             {
                 throw new ArgumentException("Invalid input");
             }
 
-
+            var salarySourcePath = args.First();
 
             //Pro lidi od roku 1950 je fixní bonus 5,-Kč
             var personBirth = new DateTime(1948, 01, 01);
