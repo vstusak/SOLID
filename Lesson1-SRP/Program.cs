6d0e9a6add7805624f7c6a72cac508d498a10200
Jiri Pazdera <JPazdera@moravia.com>
2024-08-06 09:57:18 +0200

    Refactoring - programovani od usage

--- Lesson1-SRP/Program.cs
+++ Lesson1-SRP/Program.cs
@@ -19,24 +19,28 @@ namespace Lesson1_SRP
             //{
             //    throw new ArgumentException("Application started with wrong number of parameters.");
             //}
+
+
             //if (!File.Exists(salarySourcePath))
             //{
             //    throw new ArgumentException("File doesnt exist.");
             //}
 
+            IInputValidator _validator = new InputValidator();
+            var validationResult = _validator.ValidateInput(args);
+
+            if (validationResult.IsValid())
+            {
+                throw new ArgumentException(validationResult.GetMessages());
+            }
+
+            var salarySourcePath = args.First();
+
+
             // DONE validation method for args - 1) file exists, 2) file is in json format (maybe not)
             // DONE extract method to separate class
             // TODO app run based on user input (select salaries source file while application is running)
             // TODO revert finished DONEs and implement from usage point of view
-
-            var applicationInputValidator = new ApplicationInputValidator();
-            var isApplicationInputValid = applicationInputValidator.ValidateInputArguments(args);
-            if (!isApplicationInputValid)
-            {
-                throw new ArgumentException("Invalid input");
-            }
-
-            var salarySourcePath = args.First();
 
             //Pro lidi od roku 1950 je fixní bonus 5,-Kč
             var personBirth = new DateTime(1948, 01, 01);
@@ -66,5 +70,31 @@ namespace Lesson1_SRP
             outputWriter.WriteToConsole(outputMessage);
         }
 
+    }
+
+    internal class InputValidator : IInputValidator
+    {
+        public AcademyValidationResult ValidateInput(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal interface IInputValidator
+    {
+        AcademyValidationResult ValidateInput(string[] args);
+    }
+
+    internal class AcademyValidationResult
+    {
+        public bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetMessages()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
