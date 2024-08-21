0deaf2ea173da72eec4becb6bea7ffdbfdfe92bb
Jiri Pazdera <JPazdera@moravia.com>
2024-08-21 09:28:06 +0200

    Implemented validation methods - IsValid, GetMessages

--- Lesson1-SRP/Program.cs
+++ Lesson1-SRP/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
 using Lesson1_SRP.Calculations;
 using Lesson1_SRP.Entities;
@@ -15,8 +16,6 @@ namespace Lesson1_SRP
         //  As a logged-in user I need to be able to call api endpoint to get calculation whether I will need to work during retirement.
         static void Main(string[] args)
         {
-            
-
             IInputValidator _validator = new InputValidator();
             var validationResult = _validator.ValidateInput(args);
 
@@ -31,7 +30,6 @@ namespace Lesson1_SRP
             // DONE validation method for args - 1) file exists, 2) file is in json format (maybe not)
             // DONE extract method to separate class
             // TODO app run based on user input (select salaries source file while application is running)
-            // TODO implement Input Validator (Marek q. use AcademyValidationResult bool + output var List of errors)? 
             // TODO LONGTERM next topic test driven development (TDD)
 
             //Pro lidi od roku 1950 je fixní bonus 5,-Kč
@@ -69,7 +67,7 @@ namespace Lesson1_SRP
         public AcademyValidationResult ValidateInput(string[] args)
         {
             var result = new AcademyValidationResult();
-            
+
             if (args.Length != 1)
             {
                 result.ValidationErrors.Add("Application started with wrong number of parameters.");
@@ -91,14 +89,35 @@ namespace Lesson1_SRP
 
     internal class AcademyValidationResult
     {
+        public bool IsValidProperty { get; }
+
         public bool IsValid()
         {
-            throw new NotImplementedException();
+            return !ValidationErrors.Any();
+
+            //if (ValidationErrors.Any())
+            //{
+            //    return false;
+            //}
+
+            //else
+            //{
+            //    return true;
+            //}
         }
+
+        // TODO Explain stringbuilder
 
         public string GetMessages()
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+
+            foreach (var validationError in ValidationErrors)
+            {
+                sb.AppendLine(validationError);
+            }
+
+            return sb.ToString();
         }
 
         public List<string> ValidationErrors { get; set; } = new List<string>();
