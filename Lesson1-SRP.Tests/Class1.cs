1fc677024673b8f4991d74a213264ea04ed00a50
Tereza Nekutova <TNekutova@moravia.com>
2024-09-17 09:57:48 +0200

    added MultiplicationProviderTest

--- Lesson1-SRP.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lesson1_SRP.Tests
-{
-    public class Class1
-    {
-        // TODO Ukazat prepipani projektu mezi konzolovkou a dll
-        // TODO Z tohoto projektu testovaci projekt (z nuggetu stahnout x/nunit)
-    }
-}
