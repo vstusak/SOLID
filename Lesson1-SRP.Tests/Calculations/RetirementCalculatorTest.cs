44af2fa1fa40f91126234268368aa840df7173c4
Linda Adamkova <LAdamkova@moravia.com>
2024-11-12 09:59:22 +0100

    RetirementCalculatorTest added //mocking

--- Lesson1-SRP.Tests/Calculations/RetirementCalculatorTest.cs
+++ Lesson1-SRP.Tests/Calculations/RetirementCalculatorTest.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lesson1_SRP.Calculations;
+using Lesson1_SRP.Entities;
+using Moq;
+using NUnit.Framework;
 
 namespace Lesson1_SRP.Tests.Calculations
 {
     internal class RetirementCalculatorTest
     {
-        //TO DO
+        [TestCase(10000)]
+        [TestCase(31000)]
+        public void SentByParameter_CalculateRetirementSalary_ExpectedResultByParameter(int expectedResult)
+        {
+            //arrange no default -> MockBehavior.Strict + corresponding setup
+            var bonusProviderMock = new Mock<IBonusProvider>(MockBehavior.Strict);
+            var multiplicationProviderMock = new Mock<IMultiplicationProvider>(MockBehavior.Strict);
+            
+            var underTest = new RetirementCalculator(bonusProviderMock.Object, multiplicationProviderMock.Object);
+            List<Salary> emptySalaryList = new List<Salary>();
+
+            //act
+            var actualResult = underTest.CalculateRetirementSalary(emptySalaryList);
+
+            //assert
+            NUnit.Framework.Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
     }
 }
