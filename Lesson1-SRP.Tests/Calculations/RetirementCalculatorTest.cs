441c8237eda924f61397cd9364f1d5bc31d364fd
Marek Vesely <MVesely@moravia.com>
2024-12-03 09:55:59 +0100

    setup bonusProviderMock and multiplicationProviderMock for RetirementCalculatorTest

--- Lesson1-SRP.Tests/Calculations/RetirementCalculatorTest.cs
+++ Lesson1-SRP.Tests/Calculations/RetirementCalculatorTest.cs
@@ -12,17 +12,23 @@ namespace Lesson1_SRP.Tests.Calculations
 {
     internal class RetirementCalculatorTest
     {
-        [TestCase(10000)]
-        [TestCase(31000)]
-        public void SentByParameter_CalculateRetirementSalary_ExpectedResultByParameter(int expectedResult)
+        [TestCase(10005, 1, 2, 3 )]
+        public void SentByParameter_CalculateRetirementSalary_ExpectedResultByParameter(int expectedResult, double multiplicationResult, int bonusResult1, int bonusResult2)
         {
             //arrange no default -> MockBehavior.Strict + corresponding setup
             var bonusProviderMock = new Mock<IBonusProvider>(MockBehavior.Strict);
             var multiplicationProviderMock = new Mock<IMultiplicationProvider>(MockBehavior.Strict);
-            
+            multiplicationProviderMock.Setup(mock => 
+                mock.GetMultiplication(It.IsAny<List<Salary>>())).Returns(multiplicationResult);
+            bonusProviderMock.Setup(mock => mock.GetBonuses(It.IsAny<List<Salary>>()))
+                .Returns(new List<int>() { bonusResult1, bonusResult2 });
+
             var underTest = new RetirementCalculator(bonusProviderMock.Object, multiplicationProviderMock.Object);
             List<Salary> emptySalaryList = new List<Salary>();
-
+            
+            //Retirement calculator result to be asserted
+            //return Convert.ToInt32(baseSalary * multiplication + bonuses.Sum());
+            
             //act
             var actualResult = underTest.CalculateRetirementSalary(emptySalaryList);
 
