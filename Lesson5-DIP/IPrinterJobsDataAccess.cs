fa37baaf3f3923bffe2555a61f1428244531eef3
Jana Vackova <JVackova@moravia.com>
2024-12-10 09:56:47 +0100

    Lesson5_DIP

--- Lesson5-DIP/IPrinterJobsDataAccess.cs
+++ Lesson5-DIP/IPrinterJobsDataAccess.cs
@@ -1,8 +1,18 @@
-﻿namespace Lesson5_DIP
+﻿using System.Collections.Generic;
+
+namespace Lesson5_DIP
 {
     public interface IPrinterJobsDataAccess
     {
-        SqlDataReader GetJobs(SqlParameterCollection parameters);
+        List<Job> GetJobs(SqlParameterCollection parameters);
+    }
+
+    public class PrinterJobsDataAccess: IPrinterJobsDataAccess
+    {
+        public List<Job> GetJobs(SqlParameterCollection parameters)
+        {
+            throw new System.NotImplementedException();
+        }
     }
     
     public class SqlParameterCollection
@@ -11,5 +21,12 @@
 
     public class SqlDataReader
     {
+    }
+
+    public class Job// hvězdičky
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Data { get; set; }
     }
 }
