aabd6cbf5a7394d0cb481653434ac186d531fb82
Linda Adamkova <LAdamkova@moravia.com>
2024-12-16 11:59:43 +0100

    new report project and class added

---
+++ Reports/Program.cs
@@ -0,0 +1,20 @@
+﻿// Zadání:
+// Aplikace by měla sestavovat a tisknout reporty.
+// Záloha reportu - kopie by se měla ukládat na disk.
+// Report by měl obsahovat hlavičku, data z aktuální stav skladu a datum, čas.
+// Tiskárna = console => aplikace by toto měla odesílat do tiskárny.
+
+// Vytvořit třídy:
+// 0. Definice class Report (Datová třída Report - property: header, dataCreated, body)
+// 1. ReportFactory - tvorba reportu
+//      1. a) HeaderReader class: Načíst hlavičku - pěknou, obyč. a ugly (nastavení parametrem)
+//      1. b) DataReader class: Načíst stav skladu db
+//      1. c) TimeProvider class: Nastavit (získat) aktuální datum a čas //timeProvider
+// 2. Printer - vypíše report na consoli
+// 3. Třída pro ukládání dat na disk - FileReportWritter
+// 4. Test coverage
+// 5. Refactoring -> move everything to Main
+// 6. Připojit DataReader na reálnou DB
+// 7. Nahradit tiskárnu uložením do DB
+
+Console.WriteLine("Hello, World!");
---
+++ Reports/Report.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reports
+{
+    public class Report
+    {
+        // 0. Definice class Report (Datová třída Report - property: header, dataCreated, body)
+
+        public required string Header { get; set; } //TODO: replace required by constructor
+        public required DateTime DataCreated { get; set; }
+        public required string Body { get; set; }
+
+    }
+}
