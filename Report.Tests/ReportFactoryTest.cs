f9eadfe3fe645b8534c3f9fb35c9cf3077edaf9b
Anna Radchuk <ARadchuk@moravia.com>
2025-02-13 09:24:16 +0100

    Simplify tests, tests renaming

--- Report.Tests/ReportFactoryTest.cs
+++ Report.Tests/ReportFactoryTest.cs
@@ -8,7 +8,7 @@ namespace Report.Tests
     public class ReportFactoryTest
     {
         [Test]
-        public void ReportCreationTest()
+        public void CreateReport_EmptyData_EmptyBody()
         {
             //Arrange
             var expectedDateTime = new DateTime(2025, 1, 23, 8, 55, 24, DateTimeKind.Local);
@@ -25,19 +25,41 @@ namespace Report.Tests
             var result = underTest.CreateReport(HeaderType.Employees);
 
             //Assert
-            Assert.That(result.Header, Is.EqualTo("All"));
             Assert.That(result.Body, Is.EqualTo(string.Empty));
-            Assert.That(result.DataCreated, Is.EqualTo(expectedDateTime));
         }
 
         [Test]
-        public void ReportCreationTest2()
+        public void CreateReport_DataReaderWithData_BodyInCorrectFormat()
         {
             //Arrange
             var expectedDateTime = new DateTime(2025, 1, 23, 8, 55, 24);
 
             var dataReaderMock = new Mock<IOurDataReader>(MockBehavior.Strict);
-            dataReaderMock.Setup(mock => mock.GetData()).Returns(new Dictionary<string, int>{{"chleba", 5}});
+            dataReaderMock.Setup(mock => mock.GetData()).Returns(new Dictionary<string, int> { { "chleba", 5 } });
+
+            var dateTimeProviderMock = new Mock<IDateTimeProvider>(MockBehavior.Strict);
+            dateTimeProviderMock.Setup(mock => mock.UtcNow).Returns(expectedDateTime);
+
+            ReportFactory underTest = new ReportFactory(dataReaderMock.Object, dateTimeProviderMock.Object);
+
+            //Act
+            var result = underTest.CreateReport(HeaderType.Employees);
+
+            //Assert
+            Assert.That(result.Body, Is.EqualTo($"chleba : 5{Environment.NewLine}"));
+
+            //TODO: Set correct names for tests
+            //TODO: TDD + project calculator
+        }
+        //TODO: Rename test according to the assert
+        [Test]
+        public void CreateReport_HeaderTypeEmpoyees_ReportInCorrectFormat()
+        {
+            //Arrange
+            var expectedDateTime = new DateTime(2025, 1, 23, 8, 55, 24);
+
+            var dataReaderMock = new Mock<IOurDataReader>(MockBehavior.Strict);
+            dataReaderMock.Setup(mock => mock.GetData()).Returns(new Dictionary<string, int> { { "chleba", 5 } });
 
             var dateTimeProviderMock = new Mock<IDateTimeProvider>(MockBehavior.Strict);
             dateTimeProviderMock.Setup(mock => mock.UtcNow).Returns(expectedDateTime);
@@ -49,11 +71,6 @@ namespace Report.Tests
 
             //Assert
             Assert.That(result.Header, Is.EqualTo("All"));
-            Assert.That(result.Body, Is.EqualTo($"chleba : 5{Environment.NewLine}"));
-            Assert.That(result.DataCreated, Is.EqualTo(expectedDateTime));
-
-            //TODO: Set correct names for tests
-            //TODO: TDD + project calculator
         }
     }
 }
