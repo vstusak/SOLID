178db1d5925172608744ac04dd5131d4775d20a1
Anna Radchuk <ARadchuk@moravia.com>
2025-03-21 12:53:03 +0100

    TDD - first tests, TddClalculator class

---
+++ Calculator.Tests/Class1.cs
@@ -0,0 +1,59 @@
+﻿
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTests
+    {
+        [TestCase(1, 2)]
+        [TestCase(1, 0)]
+        public void Add_InputData_DataAddedTogether(int value1, int value2)
+        {
+            //Arrange
+            var underTest = new TddCalculator();
+            //Act
+            var result = underTest.Add(value1, value2);
+            //Assert
+            var expected = value1 + value2;
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(1, 0)]
+        public void Sub_InputData_DataSubstituted(int value1, int value2)
+        {
+            //Arrange
+            var underTest = new TddCalculator();
+            //Act
+            var result = underTest.Sub(value1, value2);
+            //Assert
+            Assert.That(result, Is.EqualTo(value1 - value2));
+        }
+
+        [TestCase(0, 1)]
+        [TestCase(1, 2)]
+        public void Multiplication_InputData_DataMultiplicated(int value1, int value2)
+        {
+            //Arrange
+            var underTest = new TddCalculator();
+            //Act
+            var result = underTest.Mult(value1, value2);
+            //Assert
+            Assert.That(result, Is.EqualTo(value1*value2));
+        }
+
+        //TODO 27.03.: Handle divide by zero, do not support
+        //TODO: Assertion of double 
+        [TestCase(1, 0)]
+        public void Division_InputData_DataDivided(double value1, double value2)
+        {
+            //Arrange
+            var underTest = new TddCalculator();
+            //Act
+            var result = underTest.Div(value1, value2);
+            //Assert
+            Assert.That(result, Is.EqualTo(value1 / value2));
+        }
+    }
+}
---
+++ Calculator/TddCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class TddCalculator
+    {
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        public int Sub(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        public int Mult(int value1, int value2)
+        {
+          return   value1 * value2;
+        }
+
+        public double Div(double value1, double value2)
+        {
+
+            return value1 / value2;
+        }
+    }
+}
