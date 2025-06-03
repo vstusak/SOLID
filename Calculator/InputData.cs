555fca6d8cc552419a5ace88d76247e0baeb8300
Linda Adamkova <LAdamkova@moravia.com>
2025-06-03 13:48:45 +0200

    inputData.cs added

---
+++ Calculator/InputData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class InputData
+    {
+        public OperationEnum Operator; 
+        public double Value1;
+        public double Value2;
+    }
+}
