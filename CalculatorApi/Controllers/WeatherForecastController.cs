230f978c2080cba2de581c954e341946649267db
Anna Radchuk <ARadchuk@moravia.com>
2025-06-16 11:57:39 +0200

    experiments with body and query values sent through postman

--- CalculatorApi/Controllers/WeatherForecastController.cs
+++ CalculatorApi/Controllers/WeatherForecastController.cs
@@ -18,18 +18,19 @@ namespace CalculatorApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-                })
-                .ToArray();
-        }
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public int Get(int abc)
+        //{
 
+        //    return abc;
+        //}
+
+        [HttpGet(Name = "GetWeatherForecast2")]
+        public int TwoGet([FromBody]int abc) // bere hodnotu z Body
+        {
+
+            return abc;
+        }
     }
 
     // TODO: add POST method
