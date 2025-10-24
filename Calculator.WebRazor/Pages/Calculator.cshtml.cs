06b646ab15b929eca69d354cf6556f381bbca46f
Marek Vesely <MVesely@moravia.com>
2025-10-24 12:20:19 +0200

    add value2 and operand, make operand dropdown. Local calculation only for POC

--- Calculator.WebRazor/Pages/Calculator.cshtml.cs
+++ Calculator.WebRazor/Pages/Calculator.cshtml.cs
@@ -6,7 +6,10 @@ namespace Calculator.WebRazor.Pages
     public class CalculatorModel : PageModel
     {
         private readonly ILogger<CalculatorModel> _logger;
-        public string Hodnota1 { get; set; }
+        public int Hodnota1 { get; set; }
+        public int Hodnota2 { get; set; }
+        public string Operand { get; set; }
+        public string Result { get; set; }
 
         public CalculatorModel(ILogger<CalculatorModel> logger)
         {
@@ -21,11 +24,23 @@ namespace Calculator.WebRazor.Pages
         //{
         //    _logger.LogWarning("We are in method First post.");
         //}
-        public void OnPost(string value1)
+        public void OnPost(string value1, string value2, string operand)
         {
             _logger.LogWarning("We are in method post.");
-            _logger.LogInformation($"Value 1 is {value1}");
-            Hodnota1=value1;
+            _logger.LogInformation($"[{nameof(value1)}] is [{value1}], [{nameof(operand)}] is [{operand}], [{nameof(value2)}] is [{value2}]");
+            Hodnota1 = Int32.Parse(value1); //TODO create validations, handle errors
+            Hodnota2 = Int32.Parse(value2);
+            Operand = operand;
+
+            switch (Operand)  //TODO replace this local POC with an API call. 
+            {
+                case "+":
+                    Result = (Hodnota1 + Hodnota2).ToString();
+                    break;
+                default:
+                    Result = ($"[{nameof(Operand)}] [{Operand}] is not yet supported.");
+                    break;
+            }
         }
     }
 }
