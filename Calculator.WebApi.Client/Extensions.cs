243a1b12fe2c64d4f06e4e6ea3a936ff8d8e9c8c
Jana Vackova <JVackova@moravia.com>
2026-03-30 13:58:46 +0200

    new TODO are added

--- Calculator.WebApi.Client/Extensions.cs
+++ Calculator.WebApi.Client/Extensions.cs
@@ -8,6 +8,11 @@ namespace Calculator.WebApi.Client
         {
             services.AddHttpClient<LocalhostCalculatorApiClient>();
             //TODO: repeat implementation of fluent
+            //TODO: vysvětlit si rows collection. proč bychom měli kolekci objektů zabalit do nadřazené třídy
+            //TODO class method class params in method parameters
+            //TODO: we finished with factory, show example and refactor for builder
+            //great example suggestion - factory GetRows and get specific rows with various types
+            //TODO: we should finish with something like this: rowsBuilder.WithUserRows().WithProjectRows().WithTaskRows().GetRows()
             return services;
         }
     }
