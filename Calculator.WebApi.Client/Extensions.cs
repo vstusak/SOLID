4fadfe432ad607821624e49673ef4d496626c4ab
Jana Vackova <JVackova@moravia.com>
2026-04-23 12:18:30 +0200

    Refactor: move TODOs outside method body
    - Relocated inline TODOs to end of Extensions.cs file

--- Calculator.WebApi.Client/Extensions.cs
+++ Calculator.WebApi.Client/Extensions.cs
@@ -4,16 +4,27 @@ namespace Calculator.WebApi.Client
 {
     public static class Extensions
     {
+        //TODO: repeat implementation of fluent
         public static IServiceCollection AddCalculatorApiClients(this IServiceCollection services)
         {
             services.AddHttpClient<LocalhostCalculatorApiClient>();
-            //TODO: repeat implementation of fluent
-            //TODO: vysvětlit si rows collection. proč bychom měli kolekci objektů zabalit do nadřazené třídy
-            //TODO class method class params in method parameters
-            //TODO: we finished with factory, show example and refactor for builder
-            //great example suggestion - factory GetRows and get specific rows with various types
-            //TODO: we should finish with something like this: rowsBuilder.WithUserRows().WithProjectRows().WithTaskRows().GetRows()
             return services;
         }
     }
 }
+
+
+//TODO class method class params in method parameters
+
+
+
+
+
+//TODO: vysvětlit si rows collection. proč bychom měli kolekci objektů zabalit do nadřazené třídy
+
+
+
+
+//TODO: we finished with factory, show example and refactor for builder
+//TODO: we should finish with something like this: rowsBuilder.WithUserRows().WithProjectRows().WithTaskRows().GetRows()
+//great example suggestion - factory GetRows and get specific rows with various types
