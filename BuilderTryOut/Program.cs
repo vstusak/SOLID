381a515ea08dfcc58f28a20717487ffd5b282589
Anna Radchuk <ARadchuk@moravia.com>
2026-04-27 14:00:19 +0200

    BuilderTryOut project created

---
+++ BuilderTryOut/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Welcome in Report builder!");
+Console.WriteLine("Please choose type of report you want to create:");
+Console.WriteLine("1. Simple Report");
+Console.WriteLine("2. Full Report");
+Console.WriteLine("3. Pretty Report");
+Console.Write("Enter your choice: ");
+string choice = Console.ReadLine();
+ReportDto reportDtoResult = null;
+switch (choice)
+{
+    case "1":
+        Console.WriteLine("You chose Simple Report.");
+        reportDtoResult = new ReportBuilder().AddDate().Build();
+        break;
+    case "2":
+        Console.WriteLine("You chose Full Report.");
+        reportDtoResult = new ReportBuilder().AddDate().AddGreeting().Build();
+        break;
+    case "3":
+        Console.WriteLine("You chose Pretty Report.");
+        break;
+    default:
+        Console.WriteLine("Invalid choice.");
+        break;   
+}
+
+
+Console.WriteLine(reportDtoResult?.ToString());
+
+public class ReportDto
+{
+    public string Header { get; set; }
+    public string Body { get; set; }            
+    public string Footer { get; set; }
+    public override  string ToString()
+    {
+        return $"{Header}\n{Body}\n{Footer}";
+    }
+}
+
+public class ReportBuilder
+{
+    private readonly ReportDto _reportDto = new ReportDto();
+    public ReportBuilder AddDate()
+    {
+        _reportDto.Header = _reportDto.Header + DateTime.Now.ToString();
+        return this;
+    }
+    public ReportBuilder AddGreeting()
+    {
+        _reportDto.Header = _reportDto.Header + " - Hello!";
+        return this;
+    }
+
+    public ReportDto Build()
+    {
+        return _reportDto;
+    }
+}
--- Calculator.WebApi.Client/Extensions.cs
+++ Calculator.WebApi.Client/Extensions.cs
@@ -4,7 +4,7 @@ namespace Calculator.WebApi.Client
 {
     public static class Extensions
     {
-        //TODO: repeat implementation of fluent
+        //DONE: repeat implementation of fluent
         public static IServiceCollection AddCalculatorApiClients(this IServiceCollection services)
         {
             services.AddHttpClient<LocalhostCalculatorApiClient>();
@@ -16,15 +16,7 @@ namespace Calculator.WebApi.Client
 
 //TODO class method class params in method parameters
 
-
-
-
-
 //TODO: vysvětlit si rows collection. proč bychom měli kolekci objektů zabalit do nadřazené třídy
-
-
-
-
-//TODO: we finished with factory, show example and refactor for builder
-//TODO: we should finish with something like this: rowsBuilder.WithUserRows().WithProjectRows().WithTaskRows().GetRows()
+//DONE: we finished with factory, show example and refactor for builder
+//DONE: we should finish with something like this: rowsBuilder.WithUserRows().WithProjectRows().WithTaskRows().GetRows()
 //great example suggestion - factory GetRows and get specific rows with various types
