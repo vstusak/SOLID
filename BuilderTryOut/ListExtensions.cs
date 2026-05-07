83825adfc6bc031d379740e068922322079364e0
Jana Vackova <JVackova@moravia.com>
2026-05-07 12:16:30 +0200

    Add child lists to reports and ListExtensions
    - Add StringJoin extension for List<string>
    - Add healthy/unhealthy children lists to reports
    - Update ReportBuilder to handle new lists
    - Improve report formatting and greeting default
    - Update user choices to include child lists

---
+++ BuilderTryOut/ListExtensions.cs
@@ -0,0 +1,7 @@
+﻿public static class ListExtensions
+{
+    public static string StringJoin(this List<string> list, string separator)
+    {
+        return string.Join(separator, list);
+    }
+}
--- BuilderTryOut/Program.cs
+++ BuilderTryOut/Program.cs
@@ -7,15 +7,25 @@ Console.WriteLine("3. Pretty Report");
 Console.Write("Enter your choice: ");
 string choice = Console.ReadLine();
 ReportDto reportDtoResult = null;
+var unHealthyChildren = new List<string>() { "Honza", "Pepa" };
+var healthyChildren = new List<string>() { "Linda", "Mara" };
 switch (choice)
 {
     case "1":
         Console.WriteLine("You chose Simple Report.");
-        reportDtoResult = new ReportBuilder().AddCurrentDate().Build();
+        reportDtoResult = new ReportBuilder()
+            .AddCurrentDate()
+            .AddUnHealthyChildren(unHealthyChildren)
+            .Build();
         break;
     case "2":
         Console.WriteLine("You chose Full Report.");
-        reportDtoResult = new ReportBuilder().AddCurrentDate().UpdateGreeting().Build();
+        reportDtoResult = new ReportBuilder()
+            .AddCurrentDate()
+            .UpdateGreeting()
+            .AddUnHealthyChildren(unHealthyChildren)
+            .AddHealthyChildren(healthyChildren)
+            .Build();
         break;
     case "3":
         Console.WriteLine("You chose Pretty Report.");
@@ -47,13 +57,15 @@ public class ReportBuilder
     private string _valediction;
     private string _signature;
     private string _stamp;
+    private List<string> _healthyChildrenList = new List<string>();
+    private List<string> _unHealthyChildrenList = new List<string>();
 
     public ReportBuilder AddCurrentDate()
     {
         _date = DateTime.Now.ToString();
         return this;
     }
-    public ReportBuilder UpdateGreeting(string greeting ) //optional parameter with default value
+    public ReportBuilder UpdateGreeting(string greeting = "Hello") //optional parameter with default value
     {
         _greeting = greeting;
         return this;
@@ -63,6 +75,17 @@ public class ReportBuilder
         _address = address;
         return this;
     }
+    public ReportBuilder AddHealthyChildren(List<string> healthyChildrenList)
+    {
+        _healthyChildrenList = healthyChildrenList;
+        return this;
+    }
+    public ReportBuilder AddUnHealthyChildren(List<string> unHealthyChildrenList)
+    {
+        _unHealthyChildrenList = unHealthyChildrenList;
+        return this;
+    }
+
 
     //TODO: Finish all needed and test it + find bugs in the code and fix them
 
@@ -70,8 +93,9 @@ public class ReportBuilder
     {
         return new ReportDto
         {
-            Header = _date + _greeting + _address,
-            Body = "",
+            Header = _date+" " + _greeting + Environment.NewLine+ _address,
+            Body = "UnHealthy children: "+_unHealthyChildrenList.StringJoin(", ") +Environment.NewLine+ "Healthy children: " + _healthyChildrenList.StringJoin(", "),//extension method in ListExtensions class
+            //Body = ListExtensions.StringJoin(_unHealthyChildrenList," ") + ListExtensions.StringJoin(_healthyChildrenList, " "); public static method in ListExtensions class
             Footer = _valediction + _signature + _stamp
         };
     }
