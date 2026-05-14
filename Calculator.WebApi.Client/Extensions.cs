52f679ee05baee5ff775264108f74430b23bb3e3
Jana Vackova <JVackova@moravia.com>
2026-05-14 12:17:17 +0200

    add todo

--- Calculator.WebApi.Client/Extensions.cs
+++ Calculator.WebApi.Client/Extensions.cs
@@ -17,6 +17,7 @@ namespace Calculator.WebApi.Client
 //TODO class method class params in method parameters
 
 //TODO: vysvětlit si rows collection. proč bychom měli kolekci objektů zabalit do nadřazené třídy
+//TODO: pobavit se okolo datových typu okolo enumu
 //DONE: we finished with factory, show example and refactor for builder
 //DONE: we should finish with something like this: rowsBuilder.WithUserRows().WithProjectRows().WithTaskRows().GetRows()
 //great example suggestion - factory GetRows and get specific rows with various types
