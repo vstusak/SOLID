9b2e748f135c66ccc3f874995b040c7cb49f818d
Anna Radchuk <ARadchuk@moravia.com>
2026-05-28 12:18:33 +0200

    Recap of Calculator web razor.

--- Calculator.WebApi.Client/Extensions.cs
+++ Calculator.WebApi.Client/Extensions.cs
@@ -17,7 +17,7 @@ namespace Calculator.WebApi.Client
 //TODO class method class params in method parameters
 
 //DONE: vysvětlit si rows collection. proč bychom měli kolekci objektů zabalit do nadřazené třídy
-//TODO: pobavit se okolo datových typu okolo enumu + ukázka z FNZ
+//DONE: pobavit se okolo datových typu okolo enumu + ukázka z FNZ
 //DONE: we finished with factory, show example and refactor for builder
 //DONE: we should finish with something like this: rowsBuilder.WithUserRows().WithProjectRows().WithTaskRows().GetRows()
 //great example suggestion - factory GetRows and get specific rows with various types
--- Calculator.WebRazor/Pages/Calculator.cshtml.cs
+++ Calculator.WebRazor/Pages/Calculator.cshtml.cs
@@ -35,7 +35,8 @@ namespace Calculator.WebApp.Razor.Pages
             _logger.LogWarning("We are in method post.");
             _logger.LogInformation(
                 $"[{nameof(data.Value1)}] is [{data.Value1}], [{nameof(data.Operation)}] is [{data.Operation}], [{nameof(data.Value2)}] is [{data.Value2}]");
-            Hodnota1 = (data.Value1); //TODO create validations, handle errors
+            //TODO create validations, handle errors
+            Hodnota1 = (data.Value1); 
             Hodnota2 = (data.Value2);
             Operand = data.Operation;
 
